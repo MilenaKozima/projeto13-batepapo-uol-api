53dac6b29921065269987cf5aef04faa6a6d7a7c
MilenaKozima <mkozima06@gmail.com>
2023-08-19 22:03:46 -0300

    Configurações banco de dados

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 import express from 'express';
-import cors
+import cors from 'cors';
+import dotenv from 'dotenv';
+import { MongoClient } from 'mongodb';
 
 // criando a aplicação servidora
 const app = express();
@@ -7,6 +9,26 @@ const app = express();
 //configurações
 app.use(cors);
 app.use(express.json());
+dotenv.config();
 
+//conexão com o banco de dados
+const mongoClient = new MongoClient(process.env.DATABASE_URL)
+try{
+    mongoClient.connect()
+    console.log("MongoDB conectado")
+} catch(error){
+    console.log(error.message)
+}
+
+const db = mongoClient.db();
+
+//rotas - endpoints
+app.get("/participants", (request,response) => {
+    response.send("Tudo ok");
+})
+
+
+//ouvir na porta 5000
 const PORT = 5000;
 app.listen(PORT, () => console.log(`Server running at port ${PORT}`));
+
